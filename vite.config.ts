ae137fb5a547ef76206d7bb4fd8dd007426068b0
zappaboy <federico.zappone@justanother.cloud>
2024-11-16 20:36:52 +0100

    Updated base project dir names;

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
@@ -6,7 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
-    outDir: './docs'
+    outDir: './dist'
   },
-  base: '/vite-boilerplate/'
+  base: '/tma-flows/'
 });
